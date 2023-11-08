b830c7fc6703b19bc19f5df60dd675f190d2244d
SzymonRozwoda <72217988+SzymonRozwoda@users.noreply.github.com>
2023-11-08 21:06:14 +0100

    Add files via upload

---
+++ osoba/Osoba.java
@@ -0,0 +1,26 @@
+/**
+ * @author Szymon
+ */
+public class Osoba{
+
+    /**
+     * tu jest imie
+     */
+    String imie;
+
+    /**
+     * 
+     * @return zwraca imie
+     */
+    public String getImie(){
+        return imie;
+    }
+
+    /**
+     * 
+     * @param imie ustawia imie
+     */
+    public void setImie(String imie){
+        this.imie=imie;
+    }
+}
---
+++ osoba/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Osoba","l":"getImie()"},{"p":"<Unnamed>","c":"Osoba","l":"Osoba()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Osoba","l":"setImie(String)","u":"setImie(java.lang.String)"}];updateSearchResults();
